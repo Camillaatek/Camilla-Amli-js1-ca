1fcd300e9f33b2a2dfe1fd3a29480d9ba6e2be0a
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2021-02-28 22:22:34 +0100

    Update details.js

--- details.js
+++ details.js
@@ -1,16 +1,14 @@
 const getUrl = document.location.search
 console.log (getUrl)
 
-const para = new URLSearchParams(getUrl)
-const id = para.get("id")
+const ny = new URLSearchParams(getUrl)
+const id = ny.get("id")
 console.log (id)
-const box = document.querySelector (".details")
+const place = document.querySelector (".details")
 const url = `https://ghibliapi.herokuapp.com/films/${id}`
 console.log(window.location.href)
-
+ place.innerHTML = '<div class="load loading-icon"></div>';
 const ghibliStudio = (movie) => {
-    
-    box.innerHTML = "";
     
     let nyFilm = `
     <ul>
@@ -21,13 +19,11 @@ const ghibliStudio = (movie) => {
         <p>Rating: ${movie.rt_score}</p>
         
     </ul>`;
-        box.innerHTML += nyFilm;
+        place.innerHTML = nyFilm;
         document.title = (movie.title)
-    
-    
 };
 
- fetch(url)
+fetch(url)
     .then(response => response.json())
     .then(data => ghibliStudio(data))
     .catch(err => console.error(err.message)
